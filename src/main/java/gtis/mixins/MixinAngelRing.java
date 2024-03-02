c43234ddc3e86399b285bdbc3acf9d97366434b6
Chara_SS <chara_ss@outlook.com>
2024-03-02 15:14:45 +0800

    adjust Angel Ring's GP requirement

---
+++ src/main/java/gtis/mixins/MixinAngelRing.java
@@ -0,0 +1,28 @@
+package gtis.mixins;
+
+import com.rwtema.extrautils2.backend.XUItemFlatMetadata;
+import com.rwtema.extrautils2.items.ItemAngelRing;
+import com.rwtema.extrautils2.power.player.IPlayerPowerCreator;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+/**
+ * @author Chara_SS
+ */
+@Mixin(value = ItemAngelRing.class, remap = false)
+public abstract class MixinAngelRing extends XUItemFlatMetadata implements IPlayerPowerCreator {
+    
+    @Inject(method = "addInformation", at = @At("HEAD"), remap = false, cancellable = true)
+    public void addInformationMixin(ItemStack stack, EntityPlayer playerIn, List<String> tooltip, boolean advanced, @NotNull CallbackInfo ci){
+        super.addInformation(stack, playerIn, tooltip, advanced);
+        tooltip.add("GP消耗已被禁用.");
+        ci.cancel();
+    }
+}
--- src/main/java/gtis/mixins/MixinPowerAngelRing.java
+++ src/main/java/gtis/mixins/MixinPowerAngelRing.java
@@ -1,7 +1,11 @@
 package gtis.mixins;
 
 import com.rwtema.extrautils2.items.ItemAngelRing;
+import com.rwtema.extrautils2.power.player.PlayerPower;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,14 +15,52 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 /**
  * @author Chara_SS
  */
-@Mixin(ItemAngelRing.PlayerPowerAngelRing.class)
-public abstract class MixinPowerAngelRing {
+@Mixin(value = ItemAngelRing.PlayerPowerAngelRing.class, remap = false)
+public abstract class MixinPowerAngelRing extends PlayerPower {
 
     @Shadow(remap = false)
     int power;
 
-    @Inject(method = "update", at = @At("RETURN"), remap = false)
-    public void update(boolean selected, ItemStack params, CallbackInfo ci) {
-        this.power = 0;
+    @Shadow
+    boolean wasFlying;
+
+    @Shadow
+    String name;
+
+    @Shadow
+    int type;
+
+    public MixinPowerAngelRing(EntityPlayer referent, int type) {
+        super(referent);
+        this.name = referent.getGameProfile().getName();
+        this.type = type;
+    }
+
+    @Inject(method = "update", at = @At("HEAD"), remap = false, cancellable = true)
+    public void update(boolean selected, ItemStack params, @NotNull CallbackInfo ci) {
+        if (!this.invalid) {
+            ItemAngelRing.updateWingsDisplay(this.name, this.type, selected);
+            EntityPlayerMP player = this.getPlayerMP();
+            if (!this.getPlayer().isSpectator()) {
+                if (!player.capabilities.allowFlying) {
+                    player.capabilities.allowFlying = true;
+                    player.sendPlayerAbilities();
+                }
+
+                if (player.capabilities.isFlying) {
+                    this.wasFlying = true;
+                }
+
+                if (this.wasFlying) {
+                    if (player.onGround) {
+                        this.wasFlying = false;
+                    }
+
+                }
+                this.power = 0;
+
+            }
+        }
+        ci.cancel();
     }
 }
